5f62ae965f414bb27b239201844bd5ad851aac90
O-Haruki <134474921+O-Haruki@users.noreply.github.com>
2025-06-16 14:15:20 +0900

    add explain

--- Assets/DeathBase.cs
+++ Assets/DeathBase.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// デスベス
+/// </summary>
 public abstract class DeathBase : MonoBehaviour
 {
     [SerializeField]
